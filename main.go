86ef35bcd58ee1b74edb06de9de814bd75ea205b
Ilya Suprun <suprunis@aes.tgc-2.ru>
2022-12-04 23:16:42 +0300

    modified:   go.mod new file:   go.sum modified:   main.go deleted:    test

--- main.go
+++ main.go
@@ -4,25 +4,41 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"strings"
+	"syscall"
 
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/term"
 )
 
 func main() {
-	var hostKey ssh.PublicKey
-	// An SSH client is represented with a ClientConn.
-	//
-	// To authenticate with the remote server you must pass at least one
-	// implementation of AuthMethod via the Auth field in ClientConfig,
-	// and provide a HostKeyCallback.
-	config := &ssh.ClientConfig{
-		User: "username",
-		Auth: []ssh.AuthMethod{
-			ssh.Password("yourpassword"),
-		},
-		HostKeyCallback: ssh.FixedHostKey(hostKey),
+
+	user := "suprunis"
+	NumberOfPrompts := 3
+
+	// Normally this would be a callback that prompts the user to answer the
+	// provided questions
+	Cb := func(user, instruction string, questions []string, echos []bool) (answers []string, err error) {
+		pw, err := credentials()
+		return []string{pw}, err
 	}
-	client, err := ssh.Dial("tcp", "yourserver.com:22", config)
+
+	config := &ssh.ClientConfig{
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		User:            user,
+		Auth: []ssh.AuthMethod{
+			// ssh.RetryableAuthMethod(ssh.KeyboardInteractiveChallenge(Cb), NumberOfPrompts),
+			// ssh.Password(pass), // works
+			// ssh.PasswordCallback(credentials), // works
+			ssh.RetryableAuthMethod(ssh.PasswordCallback(credentials), NumberOfPrompts), // works
+		},
+		// HostKeyAlgorithms: []string{},
+	}
+
+	// host := "10.128.0.36:22"
+	host := "192.168.66.156:22"
+
+	client, err := ssh.Dial("tcp", host, config)
 	if err != nil {
 		log.Fatal("Failed to dial: ", err)
 	}
@@ -40,8 +56,39 @@ func main() {
 	// the remote side using the Run method.
 	var b bytes.Buffer
 	session.Stdout = &b
-	if err := session.Run("/usr/bin/whoami"); err != nil {
+	if err := session.Run("dir"); err != nil {
 		log.Fatal("Failed to run: " + err.Error())
 	}
 	fmt.Println(b.String())
+
+	// netConn, err := net.Dial("tcp", host)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// sshConn, _, _, err := ssh.NewClientConn(netConn, host, config)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// _ = sshConn
+	_, _ = Cb, NumberOfPrompts
+}
+
+func credentials() (string, error) {
+	// reader := bufio.NewReader(os.Stdin)
+
+	// fmt.Print("Enter Username: ")
+	// username, err := reader.ReadString('\n')
+	// if err != nil {
+	// 	return "", "", err
+	// }
+
+	fmt.Print("\nEnter Password: ")
+	bytePassword, err := term.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		return "", err
+	}
+
+	password := string(bytePassword)
+	return strings.TrimSpace(password), nil
 }
